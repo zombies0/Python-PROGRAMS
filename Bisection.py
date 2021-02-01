cc07064fd168e4bf230e28086a2b46881ba0c054
Ambrish <ambrish@Ambrishs-iMac-Pro.local>
2021-02-01 11:31:08 +0530

    new bisection method program is made

--- Bisection.py
+++ Bisection.py
@@ -1,90 +1,203 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import matplotlib.pyplot as plt
 
-def root_s(x1 ,x2, t):
+
+def root_s(x1, x2, t):
 
     tol_diff = 10.0
+    tol_difff = 10.0
 
-    f1 = f(x1);f2 = f(x2)
-    mm = 0.0;m = 0.0
-    xx1 = [];yy1 = []
-     
+    f1 = f(x1)
+    f2 = f(x2)
+    mm = 0.0
+    m = 0.0
+    mm2 = 0.0
+    xx1 = []
+    yy1 = []
+    xx2 = []
+    yy2 = []
+    c1 = x1
+    c2 = x2
+
     if f1 == 0.0:
-            
-        print("\nValue is found\nx = ",x1)
-        graph_print(xx1,yy1,x1)
+
+        xx1.append(float(x1))
+        yy1.append(float(f(x1)))
+        xx2.append(float(x2))
+        yy2.append(float(f(x2)))
+        print ('''Value is foundx = ''', x1)
+        graph_print(xx1, yy1, xx2, yy2, x1)
         return x1
-                  
-    elif f2 == 0.0 :
-    
-        print("\nValue is found\nx = ",x2)
-        graph_print(xx1,yy1,x2)
+    elif f2 == 0.0:
+
+        xx1.append(float(x1))
+        yy1.append(float(f(x1)))
+        xx2.append(float(x2))
+        yy2.append(float(f(x2)))
+        print ('''Value is foundx = ''', x2)
+        graph_print(xx1, yy1, xx2, yy2, x2)
         return x2
 
-    if (f1*f2) <= 0:
+    if f1 * f2 < 0:
 
-        while tol_diff > t:  
+        xx1.append(float(x1))
+        yy1.append(float(f(x1)))
+        xx2.append(float(x2))
+        yy2.append(float(f(x2)))
+
+        m = (x1 + x2) / 2.00
+        mm = f(m)
+        tol_diff = relative_diff(mm, mm2)
+
+        if mm == 0.0:
+
+            print ('x = ', m)
+            graph_printf(xx1, yy1, xx2, yy2, m)
+
+            return m
+        elif f1 * mm < 0:
+
+            x2 = m
+        else:
+
+            x1 = m
+    elif f1 * f2 > 0:
+
+        while tol_diff > t:
 
             mm2 = mm
+
+            xx1.append(float(x1))
+            yy1.append(float(f(x1)))
+
             m = (x1 + x2) / 2.00
             mm = f(m)
-            xx1.append(float(m))
-            yy1.append(float(mm))
-             
-            tol_diff = relative_diff(mm,mm2)
+
+            tol_diff = relative_diff(mm, mm2)
 
             if mm == 0.0:
 
-                print("x = ", m)
-                graph_print(xx1,yy1,m)
+                print ('x = ', m)
+                graph_printf(xx1, yy1, xx2, yy2, m)
 
                 return m
-            
-            elif (f1*mm) < 0: x2 = m               
+            elif f1 * mm < 0:
 
-            else: x1 = m                           
+                x2 = m
+            else:
 
+                x1 = m
+
+        while tol_difff > t:
+
+            mm2 = mm
+            xx2.append(float(x2))
+            yy2.append(float(f(x2)))
+
+            m = (x1 + x2) / 2.00
+            mm = f(m)
+
+            tol_diff = relative_diff(mm, mm2)
+
+            if mm == 0.0:
+
+                print ('x = ', m)
+                graph_printf(xx1, yy1, xx2, yy2, m)
+
+                return m
+            elif f2 * mm < 0:
+
+                x1 = m
+            else:
+
+                x2 = m
     else:
 
-        print("\nEnter the right value b/w postive and negative\n")
+        print ("Enter the right value b/w postive and negative")
+
         return 0
-    
-    print("\nTolerance limit reached\n x = ",m) 
-    graph_print(xx1,yy1,m)
+
+    tol_diff = 0.0
+
+    print ("Tolerance limit reachedx = ", m)
+    graph_printf(xx1, yy1, xx2, yy2, m)
 
     return m
 
-def f( x):
 
-    k = pow(x,2)+ (2.0 * x )-15.0
+def f(x):
+
+    k = pow(x, 2) + 2.00 * x - 15.0
     return k
+
 
 def relative_diff(a, b):
 
     a = abs(a)
     b = abs(b)
 
-    return abs(a-b)
+    return abs(a - b)
 
-def graph_print(xx1, yy1, m):
+
+def graph_print(
+    xx1,
+    yy1,
+    xx2,
+    yy2,
+    m,
+    ):
 
     y = f(m)
-    
-    plt.plot(xx1, yy1) 
-    plt.scatter(m, y) 
 
-    plt.plot(m, y, "-o")
-  
-    plt.xlabel('x - axis') 
-    plt.ylabel('y - axis') 
+    xf1 = xx1 + xx2
+    xf2 = yy1 + yy2
 
-    plt.title('f(x) = x^2 + 2 * x - 15') 
-    plt.annotate("  < - - y", (m, y))
+    plt.plot(xf1, xf2)
+    plt.scatter(m, y)
+
+    plt.plot(m, y, '-o')
+
+    plt.xlabel('x - axis')
+    plt.ylabel('y - axis')
+
+    plt.title('f(x) = x^2 + 2 * x - 15')
+    plt.annotate('  < - - y', (m, y))
 
     ax = plt.gca()
     ax.set_facecolor('xkcd:mint green')
-    
+
     plt.grid()
-    plt.show() 
+    plt.show()
+
+
+def graph_printf(
+    xx1,
+    yy1,
+    xx2,
+    yy2,
+    m,
+    ):
+
+    y = f(m)
+
+    plt.plot(xx1, yy1)
+    plt.plot(xx2, yy2)
+    plt.scatter(m, y)
+
+    plt.plot(m, y, '-o')
+
+    plt.xlabel('x - axis')
+    plt.ylabel('y - axis')
+
+    plt.title('f(x) = x^2 + 2 * x - 15')
+    plt.annotate('  < - - y', (m, y))
+
+    ax = plt.gca()
+    ax.set_facecolor('xkcd:mint green')
+
+    plt.grid()
+    plt.show()
 
 x1 = float(input("\nEnter the value of x1\n"))
 
